0707f0fd5ac25637f870c73ed7b1e7c6a28e6ea6
Sebastian Gibb <mail@sebastiangibb.de>
2020-01-30 10:27:34 +0100

    fix: just parse bash completion if dir is existing

--- bash/.bashrc.d/8-bash-completion.bash
+++ bash/.bashrc.d/8-bash-completion.bash
@@ -2,6 +2,8 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     source /etc/bash_completion
 fi
 
-for f in ~/.bash_completion.d/*; do
-  [ -f "${f}" ] && source "${f}"
-done
+if [ -d ~/.bash_completion.d ]; then
+    for f in ~/.bash_completion.d/*; do
+        [ -f "${f}" ] && source "${f}"
+    done
+fi
